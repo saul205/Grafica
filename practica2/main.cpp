190ad6e4564e389566ce1eedb0570c33c1a1bda7
Aleparicio <761783@unizar.es>
2020-10-06 18:46:25 +0200

    Lecutra

--- practica2/main.cpp
+++ practica2/main.cpp
@@ -1,26 +1,69 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
 
 void leer(std::string nombre){
 
-    std::fstream f(nombre);
-    std::string format, max, trash, name;
-    int width, height, maximo;
-    float color_res;
-    char c;
+    std::ifstream f(nombre);
+    std::string format;
+    std::string linea;
+    int numDatos = 0;
 
-    f >> format;
+    int width, height, color_res;
+    float maximo = 1.0f;
+    bool maxFound = false; // True si se ha encontrado el comentario MAX
 
     if(f.is_open()){
-        f >> c;
+        getline(f, format);
 
-        f >> max >> c >> name >> width >> height >> color_res;
+        while(numDatos != 2){
+            getline(f, linea);
+            if(linea[0] != '#'){                // Altura y anchura
+                if(numDatos == 0){
+                    std::istringstream iss(linea);
+                    iss >> width;
+                    iss >> height;
+                    ++numDatos;
+                } else {                        // Color resolution
+                    color_res = stoi(linea);
+                    ++numDatos;
+                }
+            } else {
+                if(linea.substr(0,5) == "#MAX="){
+                    linea = linea.substr(5);
+                    maximo = stof(linea);
+                    maxFound = true;
+                }
+            }
+        }
+
+        // Lee el resto de comentarios que puedan existir
+        int firstDigit;
+        firstDigit = f.peek();
+        while(!std::isdigit(firstDigit)){
+            getline(f, linea);
+            if(!maxFound && linea.substr(0,5) == "#MAX="){
+                linea = linea.substr(5);
+                maximo = stof(linea);
+                maxFound = true;
+            }
+        }
+
+        int elementCounter = 0;
+        // Image a();
+        float rgbR = 0, rgbG = 0, rgbB = 0;
+        for(int i = 0; i < height; ++i){
+            for(int j = 0; j < width; ++j){
+                f >> rgbR;
+                f >> rgbG;
+                f >> rgbB;
+                // a.setElement(elementCounter, rgbR, rgbG, rgbB);
+                ++elementCounter;
+            }
+        }
+        std::cout << elementCounter << std::endl;
     }
-
-    std::cout << name << "\n" << format << "\n" << max.erase(0, 4) << "\n" << width << " | " << height << "\n" << color_res;
-
-
 }
 
 int main(){
