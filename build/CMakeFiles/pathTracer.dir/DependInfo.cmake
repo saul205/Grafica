981d09f9e48c63992cbb5e69a404fb9758096dda
Aleparicio <761783@unizar.es>
2020-11-23 21:23:37 +0100

    REfactor

---
+++ build/CMakeFiles/pathTracer.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alejandro/Desktop/Grafica/lib/Basics/DotDir.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Basics/DotDir.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Basics/Ray.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Basics/Ray.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Basics/Transformation.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Basics/Transformation.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/GeometricFigures/Figure.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/GeometricFigures/Figure.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/GeometricFigures/LightSource.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/GeometricFigures/LightSource.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/GeometricFigures/Plane.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/GeometricFigures/Plane.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/GeometricFigures/Sphere.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/GeometricFigures/Sphere.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/GeometricFigures/Triangle.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/GeometricFigures/Triangle.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/GeometricFigures/TriangleMesh.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/GeometricFigures/TriangleMesh.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Imaging/Image.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Imaging/Image.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Imaging/Lab.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Imaging/Lab.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Imaging/Rgb.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Imaging/Rgb.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Imaging/ToneMapper.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Imaging/ToneMapper.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Sensor/Sensor.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Sensor/Sensor.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Structures/BoundingBox.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Structures/BoundingBox.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Structures/BoundingVolume.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Structures/BoundingVolume.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Structures/ConcurrentQueue.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Structures/ConcurrentQueue.cpp.o"
+  "/home/alejandro/Desktop/Grafica/lib/Structures/Scene.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/lib/Structures/Scene.cpp.o"
+  "/home/alejandro/Desktop/Grafica/practica3/main.cpp" "/home/alejandro/Desktop/Grafica/build/CMakeFiles/pathTracer.dir/practica3/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../lib/Basics"
+  "../lib/GeometricFigures"
+  "../lib/Imaging"
+  "../lib/Sensor"
+  "../lib/Structures"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/pathTracer.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pathTracer.dir/lib/Basics/DotDir.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Basics/Ray.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Basics/Transformation.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/GeometricFigures/Figure.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/GeometricFigures/LightSource.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/GeometricFigures/Plane.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/GeometricFigures/Sphere.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/GeometricFigures/Triangle.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/GeometricFigures/TriangleMesh.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Imaging/Image.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Imaging/Lab.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Imaging/Rgb.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Imaging/ToneMapper.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Sensor/Sensor.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Structures/BoundingBox.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Structures/BoundingVolume.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Structures/ConcurrentQueue.cpp.o"
+  "CMakeFiles/pathTracer.dir/lib/Structures/Scene.cpp.o"
+  "CMakeFiles/pathTracer.dir/practica3/main.cpp.o"
+  "pathTracer"
+  "pathTracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pathTracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
